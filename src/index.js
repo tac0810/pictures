9aaf4a55cd43a3c342784d50ae3b34ad7cbb8bcf
s.takaaki <s.takaaki@baqemono.jp>
2020-07-30 17:26:45 +0900

    update

--- src/index.js
+++ src/index.js
@@ -45,13 +45,9 @@ const renderer = new WebGLRenderer({
   alpha: true,
   preserveDrawingBuffer: true,
 });
-
-// const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.set(0, 0, 5);
-// camera.lookAt(0,3,0)
-// controls.update();
 renderer.setSize(width, height);
-renderer.setPixelRatio(4);
+renderer.setPixelRatio(6);
 const a = '#00101c';
 renderer.setClearColor(0x00101c);
 document.querySelector('#stage').appendChild(renderer.domElement);
@@ -59,22 +55,28 @@ document.querySelector('#stage').appendChild(renderer.domElement);
 function firebase() {
   const spread = 1.0;
   const pow = 0.75;
-  const spreadRatio = new Vector3(4.5, 8, 4);
-  const geometry = new TetrahedronBufferGeometry(1, 0);
-  const material = new MeshBasicMaterial({
-    color: 0xff3300,
-    // wireframe: true,
-    transparent: true,
-    opacity: 0.4,
-    blending: AdditiveBlending,
-  });
+  const spreadRatio = new Vector3(5, 8, 4);
+  // const geometry = new TetrahedronBufferGeometry(1, 0);
+  const geometry = new CircleGeometry(1 , 0);
+
 
   for (let i = 0; i < 500; i++) {
+
+    const material = new MeshBasicMaterial({
+      color: 0xff3300,
+      // wireframe: true,
+      transparent: true,
+      opacity: r3() + 0.075,
+      blending: AdditiveBlending,
+    });
+
     const cone = new Mesh(geometry, material);
 
-    cone.rotation.y = r3() * Math.PI;
+    // cone.rotation.y = r3() * Math.PI;
     // cone.rotation.x = r3() * Math.PI;
     // cone.rotation.x = r3() * Math.PI;
+
+    cone.rotation.z = -0.5;
 
     cone.position.setY(spreadRatio.y * r2());
 
@@ -84,35 +86,30 @@ function firebase() {
 
     const x = spreadRatio.x * Math.cos(theta) * r3();
     const z = spreadRatio.z * Math.sin(theta) * r3();
-    const wind = ((invertVolume ** 2 * 0.5) / volume) * 4 * calcNormal();
+    const wind = ((invertVolume ** 2 * 0.5) / volume) * 6 * calcNormal();
     cone.position.setX(x - wind - invertVolume ** volume);
-    cone.position.setZ(z - wind);
-    cone.position.setY(cone.position.y + invertVolume ** 2 * Math.random());
+    cone.position.setZ(z);
+    cone.position.setY(cone.position.y + invertVolume * 4);
 
     const v2 = new Vector3(
       cone.position.x / spreadRatio.x,
       cone.position.y / spreadRatio.y,
       cone.position.z / spreadRatio.z,
     );
-
-    // cone.scale.setScalar((spread - v2.length()) * pow * volume);
-    // cone.scale.setScalar((spread - v2.length()) );
-    // cone.scale.setScalar((spread - v2.length()**2) / spread );
-    // cone.scale.setScalar((1.5-v2.length()**2*1.5) * 0.2);
     cone.scale.setScalar(
       (spread / (spread + v2.length() ** 2 * 60)) * pow * volume,
     );
-    cone.translateY(-1 - r3());
+    cone.translateX(0.8 - r3());
+    cone.translateY(-1.1 - r3());
 
     scene.add(cone);
   }
 }
-
 function reflection() {
   const spread = 1.3;
   const pow = 0.7;
   const spreadRatio = new Vector3(4, 4, 1);
-  const geometry = new CircleGeometry(0.5);
+  const geometry = new CircleGeometry(0.5, 0);
   const material = new MeshBasicMaterial({
     color: 0xff3300,
     // wireframe: true,
@@ -127,7 +124,7 @@ function reflection() {
 
     circle.lookAt(0, 1, 0);
     circle.translateY(-1 + r3());
-    circle.translateZ(-1.9);
+    circle.translateZ(-2);
 
     circle.position.setX(spreadRatio.x * Math.cos(theta) * calcNormal());
     circle.position.setZ(spreadRatio.z * Math.sin(theta) * calcNormal());
@@ -135,7 +132,6 @@ function reflection() {
     scene.add(circle);
   }
 }
-
 function firewood() {
   const a = '#0c0202';
   const geometry = new CylinderGeometry(0.1, 0.1, 1.7, 3);
@@ -207,18 +203,58 @@ void main() {
   mesh.position.setZ(4.9);
   scene.add(mesh);
 }
+function smoke() {
+  const spread = 1;
+  const pow = 1;
+  const spreadRatio = new Vector3(3, 6, 1);
+  const geometry = new CircleGeometry(0.5, 0);
+  const a = '#cacaca';
+  const material = new MeshBasicMaterial({
+    // color: 0xff3300,
+    color: 0xcacaca,
+    // wireframe: true,
+    transparent: true,
+    opacity: 0.02,
+    depthTest: false,
+    depthWrite: false,
+    blending: AdditiveBlending,
+  });
+
+  for (let i = 0; i < 200; i++) {
+    const circle = new Mesh(geometry, material);
+
+    circle.position.setY(spreadRatio.y * r2());
+
+    const volume = Math.pow(1 - circle.position.y / spreadRatio.y, 2);
+    const invertVolume = 1.0 - volume;
+    const wind = ((invertVolume ** 2 * 0.5) / volume) * 6 * calcNormal();
+    const theta = (Math.random() * 2 - 1) * Math.PI;
+    const x = spreadRatio.x * Math.cos(theta) * r3();
+
+    circle.rotation.z = -0.5;
+
+    circle.position.setX(x - (wind - invertVolume ** volume) * 0.125);
+    circle.translateX(1);
+    circle.translateY(-2);
+    circle.translateZ(-2);
+
+    // const v2 = new Vector2(
+    //   circle.position.x / spreadRatio.x,
+    //   circle.position.y / spreadRatio.y,
+    // );
+    // circle.scale.setScalar(
+    //   // (spread / (spread + v2.length() ** 2 * 60)) * pow * volume,
+    //   1+ v2.length() * pow * volume,
+    // );
+    scene.add(circle);
+  }
+}
+
 firebase();
 reflection();
 firewood();
 glow();
-
-// function animate() {
-//   requestAnimationFrame(animate);
-//   controls.update();
-//   renderer.render(scene, camera);
-// }
-
-// animate();
+smoke();
 
 renderer.render(scene, camera);
 
